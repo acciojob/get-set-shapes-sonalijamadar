734edd30edcccc80283a04d31aeeb66ae13dbf6c
Sonali Jamadar <99808291+sonalijamadar@users.noreply.github.com>
2025-12-31 11:56:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 //complete this code
 class Rectangle {
 	constructor(width , height){
-		this.width = width;
-		this.height = height;
+		this._width = width;
+		this._height = height;
 	}
 	get width(){
-		return this.width;
+		return this._width;
 	}
 	get height(){
-		return this.height;
+		return this._height;
 	}
 	getArea(){
-		return this.width * this.height;
+		return this._width * this._height;
 	}
 	
 }
