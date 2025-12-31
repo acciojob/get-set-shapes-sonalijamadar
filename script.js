eb04e79dde1b2489ad9089cd4f6bb0bef7461d9d
Sonali Jamadar <99808291+sonalijamadar@users.noreply.github.com>
2025-12-31 11:45:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width , height){
+		this.width = width;
+		this.height = height;
+	}
+	getArea(){
+		return this.width * this.height;
+	}
+	
+}
 
-class Square extends Animal {}
+class Square extends Animal {
+	constructor(side){
+		super(side , side);
+	}
+	getPerimeter(){
+		return 4 * this.width;
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
